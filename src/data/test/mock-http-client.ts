e61db2e7fcd6716dfdcf287b67c4f57917101e24
Will Melo <w.melo.fernandes.un@gmail.com>
2021-05-21 00:15:52 -0300

    fix: fix lint errors

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -4,7 +4,7 @@ export class HttpPostClientSpy implements HttpPostClient {
   url?: string
   body?: object
 
-  async post(params: HttpPostParams): Promise<void> {
+  async post (params: HttpPostParams): Promise<void> {
     this.url = params.url
     this.body = params.body
     return Promise.resolve()
--- src/data/usecases/authentication/remote-authentication.ts
+++ src/data/usecases/authentication/remote-authentication.ts
@@ -2,10 +2,10 @@ import { HttpPostClient } from '@/data/protocols/http/http-post-client'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 
 export class RemoteAuthentication {
-  constructor(private readonly url: string, private readonly httpPostClient: HttpPostClient) {
+  constructor (private readonly url: string, private readonly httpPostClient: HttpPostClient) {
   }
 
-  async auth(params: AuthenticationParams): Promise<void> {
+  async auth (params: AuthenticationParams): Promise<void> {
     await this.httpPostClient.post({
       url: this.url,
       body: params
