e0ab05c1249d4c98300d63267365e7aacbe39b58
Will Melo <w.melo.fernandes.un@gmail.com>
2021-05-24 18:41:25 -0300

    test: ensure ValidationComposite returns the first error found

--- src/validation/validators/validation-composite/validation-composite.spec.ts
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -5,14 +5,15 @@ import faker from 'faker'
 describe('ValidationComposite', () => {
   test('Should return error if any validation fails', async () => {
     const validationSpy = new FieldValidationSpy('any_field')
+    validationSpy.error = new Error('first_error_message')
     const validationSpy2 = new FieldValidationSpy('any_field')
-    validationSpy2.error = new Error('any_error_message')
+    validationSpy2.error = new Error('second_error_message')
     const sut = new ValidationComposite([
       validationSpy,
       validationSpy2
     ])
 
     const error = sut.validate('any_field', 'any_value')
-    expect(error).toBe('any_error_message')
+    expect(error).toBe('first_error_message')
   })
 })
