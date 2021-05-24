40c04d8bcd63352c5d8fa9b6f7b9c57663777292
Will Melo <w.melo.fernandes.un@gmail.com>
2021-05-24 18:39:35 -0300

    test: ensure ValidationComposite returns error if any validation fails

---
+++ src/validation/validators/validation-composite/validation-composite.spec.ts
@@ -0,0 +1,18 @@
+import { ValidationComposite } from './validation-composite'
+import { FieldValidationSpy } from '../test/mock-field-validation'
+import faker from 'faker'
+
+describe('ValidationComposite', () => {
+  test('Should return error if any validation fails', async () => {
+    const validationSpy = new FieldValidationSpy('any_field')
+    const validationSpy2 = new FieldValidationSpy('any_field')
+    validationSpy2.error = new Error('any_error_message')
+    const sut = new ValidationComposite([
+      validationSpy,
+      validationSpy2
+    ])
+
+    const error = sut.validate('any_field', 'any_value')
+    expect(error).toBe('any_error_message')
+  })
+})
