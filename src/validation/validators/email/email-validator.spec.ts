d2d1dabf5dbcceabcf4ce4b1c4110a8a666f52c5
Will Melo <w.melo.fernandes.un@gmail.com>
2021-05-24 17:59:12 -0300

    test: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validator.spec.ts
+++ src/validation/validators/email/email-validator.spec.ts
@@ -5,7 +5,13 @@ import faker from 'faker'
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', async () => {
     const sut = new EmailValidation('email')
-    const error = sut.validate(faker.internet.email())
+    const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if email is valid', async () => {
+    const sut = new EmailValidation('email')
+    const error = sut.validate(faker.internet.email())
+    expect(error).toBeFalsy()
   })
 })
