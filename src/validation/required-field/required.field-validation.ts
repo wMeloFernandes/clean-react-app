5623665d8c36b87b44fc8b9a4d50ab5008709235
Will Melo <w.melo.fernandes.un@gmail.com>
2021-05-24 17:12:38 -0300

    feat: ensure RequiredFieldValidation returns error if field is empty

---
+++ src/validation/required-field/required.field-validation.ts
@@ -0,0 +1,10 @@
+import { FieldValidation } from '@/validation/required-field/protocols'
+import { RequiredFieldError } from '@/validation/required-field/errors'
+
+export class RequiredFieldValidation implements FieldValidation {
+  constructor(readonly field: string) { }
+
+  validate(value: string): RequiredFieldError {
+    return new RequiredFieldError()
+  }
+}
