89200d50f87ad27360c34dcab00ae4417315e0b6
Will Melo <w.melo.fernandes.un@gmail.com>
2021-05-24 18:00:05 -0300

    refactor: change default values of EmailValidation class

--- src/validation/validators/email/email-validator.spec.ts
+++ src/validation/validators/email/email-validator.spec.ts
@@ -1,16 +1,16 @@
 import { EmailValidation } from './email-validation'
 import { InvalidFieldError } from '@/validation/errors'
-import faker from 'faker'
+import faker, { fake } from 'faker'
 
 describe('EmailValidation', () => {
   test('Should return error if email is invalid', async () => {
-    const sut = new EmailValidation('email')
+    const sut = new EmailValidation(faker.random.word())
     const error = sut.validate(faker.random.word())
     expect(error).toEqual(new InvalidFieldError())
   })
 
   test('Should return falsy if email is valid', async () => {
-    const sut = new EmailValidation('email')
+    const sut = new EmailValidation(faker.random.word())
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
