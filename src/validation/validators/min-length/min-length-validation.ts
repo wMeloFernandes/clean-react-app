df988e47c314b830002e2ab83a31f9ec348c1894
Will Melo <w.melo.fernandes.un@gmail.com>
2021-05-24 18:14:29 -0300

    refactor: add equal operator to MinLengthValidation

--- src/validation/validators/min-length/min-length-validation.ts
+++ src/validation/validators/min-length/min-length-validation.ts
@@ -4,6 +4,6 @@ import { FieldValidation } from '@/validation/protocols'
 export class MinLengthValidation implements FieldValidation {
   constructor(readonly field: string, readonly minLength: number) { }
   validate(value: string): InvalidFieldError {
-    return value.length > this.minLength ? null : new InvalidFieldError()
+    return value.length >= this.minLength ? null : new InvalidFieldError()
   }
 }
