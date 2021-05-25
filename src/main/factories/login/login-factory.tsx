ccf7bfc0717f088ca36dcc22379b15c5da77493d
Will Melo <w.melo.fernandes.un@gmail.com>
2021-05-24 23:23:08 -0300

    feat: add env var to API URL

--- src/main/factories/login/login-factory.tsx
+++ src/main/factories/login/login-factory.tsx
@@ -4,7 +4,7 @@ import { makeRemoteAuthentication } from '@/main/factories/authentication/remote
 import { makeLoginValidation } from './login-validation-factory'
 
 export const makeLogin: React.FC = () => {
-  const url = 'http://fordevs.herokuapp.com/api/login'
+  const url = 'process.env.API_URL/login'
   return (
     <Login
       authentication={makeRemoteAuthentication(url)}
